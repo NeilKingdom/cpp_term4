d8aac5cadfd9ffbf0e8f29bf456541b1eb9a3641
NeilKingdom <kingdomneil@gmail.com>
2021-11-01 10:33:54 -0400

    Added labs up until Lab6

---
+++ Lab1/Week1_Lab1/Week1.h
@@ -0,0 +1,9 @@
+//
+// Created by neil on 9/8/21.
+//
+
+#pragma once
+#ifndef WEEK1_WEEK1_H
+#define WEEK1_WEEK1_H
+
+#endif //WEEK1_WEEK1_H
---
+++ Lab1/Week1_Lab1/cmake-build-debug/CMakeFiles/Neil_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Neil_Lab1.dir/Week1.cpp.o"
+  "Neil_Lab1"
+  "Neil_Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Neil_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
